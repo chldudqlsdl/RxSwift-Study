21901d470950aa6b2b2a4305dc3fb760a283e755
BrownOdyssey <83645833+chldudqlsdl@users.noreply.github.com>
2024-05-01 20:44:17 +0900

    2일차

---
+++ code/step3+Rx/RxSwift+MVVM/Model/Menu.swift
@@ -0,0 +1,15 @@
+//
+//  Menu.swift
+//  RxSwift+MVVM
+//
+//  Created by Youngbin Choi on 5/1/24.
+//  Copyright © 2024 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+struct Menu {
+    let name: String
+    let price: Int
+    let count: Int
+}
